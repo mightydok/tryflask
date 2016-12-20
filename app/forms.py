a0b571c932c99c85bc477ab0170030b93456c925
Vitaliy Okulov <vitaliy.okulov@gmail.com>
2016-12-21 01:43:16 +0300

    Flask play after part 3

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+from flask_wtf import FlaskForm
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
+
+class LoginForm(FlaskForm):
+    openid = TextField('openid', validators= [Required()])
+    remember_me = BooleanField('remember_me', default=False)
